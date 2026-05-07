03beb6d0786fddee6ac1ffc6a26316a953d0e773
Warren Holley <hello@warrengholley.com>
2026-05-07 14:40:08 -0600

    ... Making progress. Awaiting books or revelation to get the goddamn Device Tree Overlay nonsense to work.

--- 3-PWM-Test/wgh_pwm_module.c
+++ 3-PWM-Test/wgh_pwm_module.c
@@ -1,6 +1,6 @@
 #include <linux/module.h>
 #include <linux/init.h>
-
+#include <linux/platform_device.h>
 #include <linux/pwm.h> // PWM Interface.
 
 // Set up the Kernel Module Data.
@@ -15,69 +15,96 @@ MODULE_LICENSE("GPL");
 //  That plan was a failure, as apparently the pwm_request system was deprecated, in favor of using the Device Tree.
 //  Which, at time of writing, was giving me hell in trying to make work.
 
-// --------------------------------------------------
+// pwm-device -> Just needs a device Struct.
+// struct pwm_device *pwm_get(struct device *dev, const char *con_id)
+// Device Struct loaded from Platform Module loading procedure?
+// Might be able to get the Device handle directly, as the Platform Module nonsense appears to be mostly for I/O sub-handling.
 
+// --------------------------------------------------
 uint32_t pwm0_strength = 33; // Percent. Kernel uses nanosections.
 uint32_t pwm1_strength = 66; // Just calculate it in-script rather than have a bunch of zeroes.
 
-// pwm_get deprecated. No longer included in the pwm.h library.
-// struct pwm_device *pwm0 = NULL; // Use both on the board. Pins 32+33, or GPIO 12+13.
-// struct pwm_device *pwm1 = NULL; 
-
-// Apparently needs a generic device to request it, which is to pull from the Device Tree.
-// ... which is becoming a system that I'm starting to despise it's opaqueness.
-struct device *pwm0_d = NULL;
-struct device *pwm1_d = NULL;
-
-// --------------------------------------------------
-// ------ From here down it's all broken ------------
-// --------------------------------------------------
-
+// Just do PWM0 for now.
+struct pwm_device *pwm0_dev = NULL;
+//struct pwm_device *pwm1_dev = NULL;
 
 // --------------------------------------------------
 
-static void load_pwm_module(void){
-  // All broken. pwm_request was deprecated in favor of pwm_get, but that requires
-  //  a Device handle, which needs to be pulled from the Device Tree?
-  
-  // Just load things up. Don't faff about checking statuses.
-  pwm0 = pwm_request(0, "low_power_led");
-  pwm1 = pwm_request(1, "mid_power_led");
+static void load_pwm_module(void)
+{ 
+  // Given the pwm0 struct loaded, just configure & enable here.
   
   uint32_t pwm0_on_time = pwm0_strength * 1000000;
-  uint32_t pwm1_on_time = pwm1_strength * 1000000;
+  uint32_t pwm1_on_time = pwm1_strength * 1000000; // Not used.
   
-  pwm_config(pwm0, pwm0_on_time, 1000000000);
-  pwm_config(pwm1, pwm1_on_time, 1000000000);
+  pwm_config(pwm0_dev, pwm0_on_time, 1000000000);
+  // pwm_config(pwm1, pwm1_on_time, 1000000000);
   
-  pwm_enable(pwm0); //WGH:Research - Check if the config can be changed after enabling.
-  pwm_enable(pwm1);
+  pwm_enable(pwm0_dev); //WGH:Research - Check if the config can be changed after enabling.
+  // pwm_enable(pwm1);
 }
 
 static void unload_pwm_module(void){
-  pwm_disable(pwm0);
-  pwm_disable(pwm1);
-  
-  pwm_free(pwm0);
-  pwm_free(pwm1);
+  pwm_disable(pwm0_dev);
+  // pwm_disable(pwm1);
 }
 
-// --------------------------------------------------
+// ------ Platform Driver Nonsense to get the pwm_device --------------------------------
+
+static int dev_probe(struct platform_device *pdev)
+{ 
+  struct device *dev = &pdev->dev; // Extract the generic 'device' struct.
+  
+  // Don't need to handle any of the File-ops or misc-device registration, so just skip the my_dev_data struct used in the GPIO experiment.
+  
+  // And get the pwm_device from the generic device, 
+  pwm0_dev = pwm_get( dev, "pwm0" ); //Not sure how to expand this to both, unless using a different fragment?
+
+  return 0; //Return Success
+}
+
+static void dev_remove(struct platform_device *pdev)
+{
+  // Just need to release the PWM device.
+  pwm_put( pwm0_dev );
+}
 
 
-static int __init init_module_Test(void) {
+
+static const struct of_device_id wgh_device_ids[] = { // Configure overlay compatible.
+  { .compatible = "wgh,pwm_experiment"}, //Not sure how it finds this without a name, unless it's assumed as the KO file's in the same folder.
+  {} 
+  //Empty set marks the end of the list.
+};
+MODULE_DEVICE_TABLE( of, wgh_device_ids ); // Load the overlay.
+
+static struct platform_driver wgh_pwm_driver = {
+  .probe  = dev_probe,
+  .remove = dev_remove,
+  .driver = {
+    .name = "wgh_pwm_driver",
+    .of_match_table = wgh_device_ids,
+  }
+};
+
+// ------ Module Point --------------------------------
+// Apparently need to only run the following code once, so do the following?
+// Preprocessor? I can't recall the name.
+// Save the __init and __exit points, otherwise load the module platform driver.
+#if 1
+static int __init wgh_init_module(void) {
   printk("Loading PWM-test Module...\n");
   load_pwm_module();
   return 0;
 }
-
-static void __exit exit_module_Test(void) {
+static void __exit wgh_exit_module(void) {
   printk("Exiting PWM-test Module...\n");
   unload_pwm_module();
 }
-
-module_init(init_module_Test);
-module_exit(exit_module_Test);
-	
+module_init( wgh_init_module );
+module_exit( wgh_exit_module );
+#else
+module_platform_driver( wgh_pwm_driver ); // Not sure why this has to be filtered out.
+#endif
 	 
 
